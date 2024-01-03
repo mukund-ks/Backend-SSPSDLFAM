6023a1f153ffaa3eb892004b2212ac6b6f044f2a
Mukund Kumar <sankhyan.mukund@gmail.com>
2024-01-03 16:08:54 +0530

    Fix modules import

--- src/model.py
+++ src/model.py
@@ -1,7 +1,7 @@
 import torch
 import torch.nn as nn
 import torchvision.models as models
-from modules import ASPPModule, DecoderModule, SEModule
+from .modules import ASPPModule, DecoderModule, SEModule
 from typing import Any
 
 
